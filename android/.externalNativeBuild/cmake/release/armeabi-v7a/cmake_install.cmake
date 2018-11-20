4ca5d8f6a2c0d2ac1996850f1227239f6006ef0c
neo <neo>
2018-11-20 16:38:07 +0800

    fix and use new rule for feature-compare.

--- android/.externalNativeBuild/cmake/release/armeabi-v7a/cmake_install.cmake
+++ android/.externalNativeBuild/cmake/release/armeabi-v7a/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /react-native/AttendanceClient/node_modules/react-native-feature-compare/android
+# Install script for directory: D:/work/android/STUDY/react-native-module/react-native-feature-compare/android
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Project")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/react-native/AttendanceClient/node_modules/react-native-feature-compare/android/.externalNativeBuild/cmake/release/armeabi-v7a/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/work/android/STUDY/react-native-module/react-native-feature-compare/android/.externalNativeBuild/cmake/release/armeabi-v7a/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
